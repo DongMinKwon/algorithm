e9c022e3ec76b609a770dd526749d0706cf7ef32
DongMinKwon <wt0630@naver.com>
2023-01-17 22:48:32 +0900

    10814

---
+++ test.py
@@ -0,0 +1,14 @@
+import sys
+
+t = int(input())
+list = []
+for i in range(t):
+  [age, name] = sys.stdin.readline().split()
+  list.append([int(age), name])
+
+sorted_list = sorted(list, key=lambda x: x[0])
+
+for i in sorted_list:
+  print(i[0], i[1])
+
+  
